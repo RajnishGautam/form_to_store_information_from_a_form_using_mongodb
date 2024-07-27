8fa378f610924b23600198ec5254acbf6f6f0986
Rajnish Kumar <119472379+RajnishGautam@users.noreply.github.com>
2024-07-27 21:19:30 +0530

    Add files via upload

---
+++ formusingmongodb/models/Suggestions.js
@@ -0,0 +1,17 @@
+//formusingmongodb>models>Suggestions.js
+const mongoose=require('mongoose');
+const rschema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    suggestion:{
+        type:String,
+        required:true
+    }
+});
+module.exports=mongoose.model('Suggestions',rschema);
